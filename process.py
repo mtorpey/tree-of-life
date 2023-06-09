a4a294753ca68533fe5991bca7d47a64e49d8f84
Michael Young <mct25@st-andrews.ac.uk>
2023-06-09 22:38:28 +0100

    Add tree compression

--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import re, requests, sys
 from pypersist import persist
 
 API_URL = "https://species.wikimedia.org/w/api.php"
+COMPRESS_SEPARATOR = " → "
 
 links = dict()
 lines = [line.strip() for line in open("tree-data.txt", "r")]
@@ -24,33 +25,40 @@ def make_tree(links, start):
         tree[child] = make_tree(links, child)
     return tree
 
+def compress_tree(tree, show_all=True):
+    while True:
+        new_tree = tree.copy()
+        changed = False
+        for child_key, child_value in tree.items():
+            if child_value is not None and len(child_value) == 1:
+                grandchild_key, grandchild_value = list(child_value.items())[0]
+                if grandchild_value is not None:
+                    del new_tree[child_key]
+                    if show_all:
+                        new_key = child_key + COMPRESS_SEPARATOR + grandchild_key
+                    else:
+                        new_key = grandchild_key  # only display most specific taxon
+                    new_tree[new_key] = grandchild_value
+                    changed = True
+        tree.clear()
+        tree.update(new_tree)
+        if not changed:
+            break
+    
+    for child in tree:
+        if tree[child] is not None:
+            compress_tree(tree[child], show_all)
+
 def print_tree(tree, indent=0, name_lookup=dict(), html=False, output=print):
     for child in tree:
-        scientific = child.replace("_", " ")
-        scientific_display = scientific
-        if tree[child] is None:
-            scientific_display = abbrev_species_name(scientific_display)
-        if scientific in name_lookup:
-            common_name = name_lookup[scientific]
-        elif (api_common_name := get_common_name(scientific)) is not None:
-            common_name = api_common_name
-            name_lookup[scientific] = api_common_name  # for future runs
-        else:
-            common_name = None
+        display_string = typeset_taxon_chain(child.replace("_", " "), name_lookup, html)
         if html:
-            if common_name is None:
-                common_html = ""
-            else:
-                common_html = f'<span class="common">{common_name}</span> '
-            html_name = common_html + f'<span class="scientific">{scientific_display}</span>'
-                
             if tree[child] is None:
-                line = "<div>" + html_name + "</div>\n"
+                line = "<div>" + display_string + "</div>\n"
             else:
-                line = "<details open>\n<summary>" + html_name + "</summary>\n"
+                line = "<details open>\n<summary>" + display_string + "</summary>\n"
         else:
-            display_name = (common_name + " - " if common_name else "") + scientific_display
-            line = indent * "│ " + display_name
+            line = indent * "│ " + display_string
         output(line)
         if tree[child] is not None:
             print_tree(tree[child], indent + 1, name_lookup, html, output)
@@ -58,12 +66,43 @@ def print_tree(tree, indent=0, name_lookup=dict(), html=False, output=print):
             if tree[child] is not None:
                 output("</details>\n")
 
+def typeset_taxon_chain(chain, name_lookup, html):
+    taxa = chain.split(COMPRESS_SEPARATOR)
+    outputs = [typeset_taxon(taxon, name_lookup, html) for taxon in taxa]
+    return COMPRESS_SEPARATOR.join(outputs)
+
+def typeset_taxon(scientific, name_lookup, html):
+    scientific_display = abbrev_species_name(scientific)
+    if scientific in name_lookup:
+        common_name = name_lookup[scientific]
+    elif (api_common_name := get_common_name(scientific)) is not None:
+        common_name = api_common_name
+        name_lookup[scientific] = api_common_name  # for future runs
+    else:
+        common_name = None
+
+    if html:
+        if common_name is None:
+            common_html = ""
+        else:
+            common_html = f'<span class="common">{common_name}</span> '
+        return common_html + f'<span class="scientific">{scientific_display}</span>'
+    else:
+        return f"{common_name} ({scientific_display})" if common_name else scientific_display
+
 def abbrev_species_name(name):
-    try:
-        genus, species = name.split(" ")
-        return genus[0] + ". " + species
-    except ValueError:
-        return name
+    parts = name.split(" ")
+
+    if len(parts) == 2:
+        genus, species = parts
+        if genus.istitle() and species.islower():
+            return genus[0] + ". " + species
+    elif len(parts) == 3:
+        genus, species, subspecies = parts
+        if genus.istitle() and species.islower() and subspecies.islower():
+            return genus[0] + ". " + species[0] + ". " + subspecies
+        
+    return name
 
 def pruned_tree(tree, leaves):
     if tree is None:
@@ -80,12 +119,7 @@ def pruned_tree(tree, leaves):
         return None
     return pruned
 
-@persist(
-    key=lambda s: s,
-    hash=lambda s: s,
-    pickle=lambda n: n if n else "NO_COMMON_NAME",
-    unpickle=lambda v: None if v == "NO_COMMON_NAME" else v
-)
+@persist
 def get_common_name(species):
     # Parameters for the API request
     params = {
@@ -142,6 +176,8 @@ if targets:
 tree = {top: make_tree(links, top)}
 if targets:
     tree = pruned_tree(tree, targets)
+
+compress_tree(tree, show_all=True)
 
 print_tree(tree, name_lookup=name_lookup)
 
